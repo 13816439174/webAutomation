b5bcf6d5c8bbdcee41d5f54963327c41bca665c0
olivia <415610507@qq.com>
2021-03-11 15:49:10 +0800

    7th time commit

---
+++ report/__init__.py
@@ -0,0 +1,4 @@
+# _*_ coding:utf-8 _*_
+# 作者：Olivia
+# @Time: 2021/3/11 下午3:47
+# @File: __init__.py.py
---
+++ report/pytestReport.py
@@ -0,0 +1,13 @@
+# _*_ coding:utf-8 _*_
+# 作者：Olivia
+# @Time: 2021/3/11 下午3:48
+# @File: pytestReport.py
+import pytest
+import os
+
+if __name__ == '__main__':
+    # current_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
+    # pytest.main(['-v','../test_case/test_philipsAdmin.py','--html=../test_report/pytestReport'+current_time+'.html'])
+
+    pytest.main(['-v','../test_case/test_philipsAdmin.py','--alluredir','../temp'])
+    os.system('allure generate ../temp -o ../test_report --clean')
